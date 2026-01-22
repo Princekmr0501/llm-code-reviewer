6d9dbae6487844945a52288684c635f83e421bbe
Prince <Prince@gmail.com>
2026-01-22 19:57:55 +0530

    working on how to collect daa for review

--- backend/app.js
+++ backend/app.js
@@ -30,6 +30,7 @@ app.get("/check", (req, res) => {
                 return res.status(500).send("cannot read directory")
             }
             const results = []
+
             //now looping through the folders and checking if a `.git` file is inside it or not
             let pending = items.length;
 
@@ -69,8 +70,55 @@ app.get("/check", (req, res) => {
 
     })
 })
+//Now catch the path and action  of a git repo and access the request
+app.get("/git_repo/:action", (req, res) => {
+    const git_path = req.query.path
+    const action = req.params.action
+    if (!git_path) {
+        return res.status(400).send("Path required")
+    }
+
+    if (action === "commit") {
+        //Now go the folder and store the stdoutput insid two varibles one for commit and one for suggestion 
+        execFile("git", ["diff", "--staged"], { cwd: git_path }, (err, stdout) => {
+            if (err) {
+                return res.status(400).send("Git action failed")
+            }
+            return res.json({ type: commit, commit: stdout })
+        })//exec fie 
+    }
+    else if (action === "review") {
+        const base = req.query.base
+        const compare = req.query.compare
+        if (!base || !compare) {
+            return res.status(400).send("base and compare branch required")
+        }
+        execFile("git", ["diff", '${base}..${compare}'], { cwd: git_path }, (err, stdout) => {
+            if (err) {
+                return res.status(400).send("Git action failed")
+            }
+            return res.json({ type: review, commit: stdout })
+        })
+    }
+ else {
+            return res.status(400).send("Invalid action")
+        }
+    })
 
 
+
+
+
+
+
+
+
+
+
+//Now connect to api using a key
+//Now make prompts for review  and commit respectively
+//if commit is choosed send the commit_stdoutput + prompt to the ai
+//now get back the response 
 
 
 
